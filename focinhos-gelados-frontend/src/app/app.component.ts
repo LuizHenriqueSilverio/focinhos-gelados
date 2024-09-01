4b1ef2defd56ae2323799b9a6b905bec0b0f0a8d
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2024-09-01 16:53:48 -0300

    Páginas inicial, quem somos, doações e fale conosco finalizadas.

--- focinhos-gelados-frontend/src/app/app.component.ts
+++ focinhos-gelados-frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppToolbarComponent } from './components/app-toolbar/app-toolbar.component';
+import { AppFooterComponent } from './components/app-footer/app-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppToolbarComponent, AppFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
